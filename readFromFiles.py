a650a0e64e4d9a53724a5d92e1a7441734dd5733
KASA <kasasachith@gmail.com>
2025-08-25 09:12:47 +0530

    reading from all different file types

--- readFromFiles.py
+++ readFromFiles.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
+#read from csv file
 df = pd.read_csv("sampleData.csv", encoding="utf-8") #here encoding is not necessary and no only utf-8 but there is another that is "latin1"
+
+#read from excel file 
+# df = pd.read_excel("filename here")
+
+#read from json file
+# df = pd.read_json("filename here")
 
 print(df)
 
